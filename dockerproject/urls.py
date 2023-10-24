36d4420aa9f1151c611166e1b0c2b6faed0cbb98
sandramachado1986 <machadosandra1986@gmail.com>
2023-10-24 15:36:17 -0300

    vista

--- dockerproject/urls.py
+++ dockerproject/urls.py
@@ -13,9 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from Modulos.Academica import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('index/', views.index, name='index'),
+   
+
 ]
