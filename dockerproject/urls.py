77c8aa77c17c0c4d964e98ab39987fcb7b9af105
sandramachado1986 <=machadosandra1986@gmail.com>
2023-10-27 14:08:52 -0300

    vista agregada

--- dockerproject/urls.py
+++ dockerproject/urls.py
@@ -23,7 +23,7 @@ from Modulos.Academica import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/', views.index, name='index'),
+    path('', views.index, name='index'),
    
 
 ]
