e8fd07c02c113829dcb7c1c5248d92907083a555
radio <sandra@mdp.edu.ar>
2023-11-02 15:16:33 -0300

    campo editable

--- Modulos/Academica/admin.py
+++ Modulos/Academica/admin.py
@@ -2,15 +2,18 @@ from django.contrib import admin
 from django.contrib.auth.models import User  # Importa el modelo de usuario de Django
 
 from Modulos.Academica.models import *
-
-
 # Register your models here.
 admin.site.register(Carrera)
 admin.site.register(Correlativas)
-
-admin.site.register(Curso)
-admin.site.register(Estudiante)
 admin.site.register(Matricula)
-
-
-
+admin.site.register(Estudiante)
+@admin.register(Curso)
+class CursoAdmin (admin.ModelAdmin):
+    
+    list_display =('nombre','creditos')
+    #ordering =('nombre',);
+    #search_fields=('nombre',);
+    list_editable = ['nombre']
+    list_display_links = None  # Set list_display_links to None to allow 'nombre' to be editable
+    #list_filter=('creditos',);
+    #list_per_page = 3;
--- dockerproject/settings.py
+++ dockerproject/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
 ]
 
 ROOT_URLCONF = 'dockerproject.urls'
@@ -110,9 +111,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-ES'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Madrid'
 
 USE_I18N = True
 
